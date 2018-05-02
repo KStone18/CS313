19b58a62671ee2d095a18850d1ef64dfcdb11665
Klenton Stone <sto10026@byui.edu>
2018-05-02 16:05:00 -0600

    adding assignments page

---
+++ web/assignmentsPage.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html> 
+<html class="body1">
+<head> 
+       <meta charset="utf-8" />
+       <title> Klenton's Home Page </title>
+       <link rel="stylesheet" type="text/css" href="homepage.css"/>
+       <script type="text/javascript" src="homepage.js"></script>
+
+</head>
+	
+<body>
+
+	<div class="top">
+	     <div class="page"> Home Page
+	     	  <a href="assignmentsPage.php" class="assign">Assignments</a>
+		     </div>
+
+		     </div>
+		     
+</body>
+</html>	
