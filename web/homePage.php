046f1fbf350eb3468395cdf04438781cc44b381b
Klenton Stone <sto10026@byui.edu>
2018-05-02 17:43:15 -0600

    adding updated php

--- web/homePage.php
+++ web/homePage.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8" />
 	<title> Klenton's Home Page </title>
 	<link rel="stylesheet" type="text/css" href="homepage.css"/>
-	<script type="text/javascript" src="homepage.js"></script>
+	
 
 </head>
 	
