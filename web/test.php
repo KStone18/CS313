1c8f9637485dca4afa8988c9036bab0a4b17fcf8
Klenton Stone <sto10026@byui.edu>
2018-05-02 13:14:03 -0600

    trying to add image into header

--- web/test.php
+++ web/test.php
@@ -1,10 +1,29 @@
 <!DOCTYPE html>
 <html>
-<head> 	</head>
+<head> 
+
+</head>
 
 <body>
 	<p> this is a php page </p>
 	<?php>
+	for ($x = 10; $x < 10; $x--)
+	{
+		$color;
+		if ($x % 2 == 0)
+		{
+			$color = "red"
+		}
+		else 
+		{
+			$color = "blue"
+		}
+
+		echo "<div id =\"div$x\" class> 
+		<p>here is a div</p>
+		</div>";
+	}
+	
 	$x = 4 + "cat";
 	echo "<p>$x</p>";
 	?>
