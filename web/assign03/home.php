e3a48dbd54f0621649359e2b85c835cc78e66e71
Klenton Stone <sto10026@byui.edu>
2018-05-10 11:49:02 -0600

    adding image for cart

--- web/assign03/home.php
+++ web/assign03/home.php
@@ -7,14 +7,14 @@
   <div class="screen">
     <div class="container">
       <ul>
-        <li> <div class="title">Item 1</div> </li>
-        <li> <div class="title">Item 2</div> </li>
-        <li> <div class="title">Item 3</div> </li>
-        <li> <div class="title">Item 4</div> </li>
-        <li> <div class="title">Item 5</div> </li>
-        <li> <div class="title">Item 6</div> </li>
-        <li> <div class="title">Item 7</div> </li>
-        <li> <div class="title">Item 8</div> </li>
+        <li> <div class="title">Item 1</div> <img src="car1.png" alt="" class="image" /></li>
+        <li> <div class="title">Item 2</div> <img src="car2.png" alt="" class="image" /> </li>
+        <li> <div class="title">Item 3</div> <img src="Boat.png" alt="" class="image" /></li>
+        <li> <div class="title">Item 4</div> <img src="Jet.png" alt="" class="image" /></li>
+        <li> <div class="title">Item 5</div> <img src="excavator.png" alt="" class="image" /></li>
+        <li> <div class="title">Item 6</div> <img src="Quadcopter.png" alt="" class="image" /></li>
+        <li> <div class="title">Item 7</div> <img src="tank.png" alt="" class="image" /></li>
+        <li> <div class="title">Item 8</div> <img src="sub.png" alt="" class="image" /></li>
       </ul>
     </div>
   </div>
