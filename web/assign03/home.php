265b5b474d7b3ed7bc71c8f47c82ba25ba2ac624
Klenton Stone <sto10026@byui.edu>
2018-05-10 10:24:26 -0600

    adding php

--- web/assign03/home.php
+++ web/assign03/home.php
@@ -7,18 +7,14 @@
   <div class="screen">
     <div class="container">
       <ul>
-        <li> <div class="title">Header</div> </li>
-        <li> <div class="title">Header</div> </li>
-        <li> <div class="title">Header</div> </li>
-        <li> <div class="title">Header</div> </li>
-        <li> <div class="title">Header</div> </li>
-        <li> <div class="title">Header</div> </li>
-        <li> <div class="title">Header</div> </li>
-        <li> <div class="title">Header</div> </li>
-        <li> <div class="title">Header</div> </li>
-        <li> <div class="title">Header</div> </li>
-        <li> <div class="title">Header</div> </li>
-        <li> <div class="title">Header</div> </li>
+        <li> <div class="title">Item 1</div> </li>
+        <li> <div class="title">Item 2</div> </li>
+        <li> <div class="title">Item 3</div> </li>
+        <li> <div class="title">Item 4</div> </li>
+        <li> <div class="title">Item 5</div> </li>
+        <li> <div class="title">Item 6</div> </li>
+        <li> <div class="title">Item 7</div> </li>
+        <li> <div class="title">Item 8</div> </li>
       </ul>
     </div>
   </div>
