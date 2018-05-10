b8bb7493745dea8c7e5cee71663618a5d33949f5
Klenton Stone <sto10026@byui.edu>
2018-05-10 10:19:10 -0600

    adding php

--- web/assign03/home.php
+++ web/assign03/home.php
@@ -3,10 +3,18 @@
 <head>
  <title> Something interesting </title>
   <?php include('header.php'); ?>
-
+  <div class="pTitle"> </div>
   <div class="screen">
     <div class="container">
       <ul>
+        <?php 
+          $num = 9;
+          for($x = 1; $x != $num; $x++)
+          {
+            echo "<li> <div class=\"title\">Photo$x</div> </li>"
+          }
+        ?>
+        <!--<li> <div class="title">Header</div> </li>
         <li> <div class="title">Header</div> </li>
         <li> <div class="title">Header</div> </li>
         <li> <div class="title">Header</div> </li>
@@ -17,8 +25,7 @@
         <li> <div class="title">Header</div> </li>
         <li> <div class="title">Header</div> </li>
         <li> <div class="title">Header</div> </li>
-        <li> <div class="title">Header</div> </li>
-        <li> <div class="title">Header</div> </li>
+        <li> <div class="title">Header</div> </li>-->
       </ul>
     </div>
   </div>
