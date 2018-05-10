7c9ae5355203e8396fb6ec2505d42456973b6ed0
Klenton Stone <sto10026@byui.edu>
2018-05-10 12:18:47 -0600

    adding image for cart

--- web/assign03/home.php
+++ web/assign03/home.php
@@ -9,42 +9,42 @@
       <ul>
         <li> 
           <div class="title">Item 1</div> 
-          <img src="car1.png" alt=""  height="195" class="image" />
+          <img src="car1.png" alt=""  height="198" class="image" />
           <button type="button" class="btn btn-primary btn-xs ">Add to Cart</button>
         </li>
         <li> 
           <div class="title">Item 2</div> 
-          <img src="car2.png" alt="" height="195" class="image" /> 
+          <img src="car2.png" alt="" height="198" class="image" /> 
           <button type="button" class="btn btn-primary btn-xs">Add to Cart</button>
         </li>
         <li> 
           <div class="title">Item 3</div> 
-          <img src="Boat.png" alt="" height="195" class="image" />
+          <img src="Boat.png" alt="" height="198" class="image" />
           <button type="button" class="btn btn-primary btn-xs">Add to Cart</button>
         </li>
         <li> 
           <div class="title">Item 4</div> 
-          <img src="Jet.png" alt="" height="260" class="image" />
+          <img src="Jet.png" alt="" height="198" class="image" />
           <button type="button" class="btn btn-primary btn-xs">Add to Cart</button>
         </li>
         <li> 
           <div class="title">Item 5</div> 
-          <img src="excavator.png" alt="" height="260" class="image" />
+          <img src="excavator.png" alt="" height="198" class="image" />
           <button type="button" class="btn btn-primary btn-xs">Add to Cart</button>
         </li>
         <li> 
           <div class="title">Item 6</div> 
-          <img src="Quadcopter.png" alt="" height="260" class="image" />
+          <img src="Quadcopter.png" alt="" height="198" class="image" />
           <button type="button" class="btn btn-primary btn-xs">Add to Cart</button>
         </li>
         <li> 
           <div class="title">Item 7</div> 
-          <img src="tank.png" alt="" height="260" class="image" />
+          <img src="tank.png" alt="" height="198" class="image" />
           <button type="button" class="btn btn-primary btn-xs">Add to Cart</button>
         </li>
         <li> 
           <div class="title">Item 8</div> 
-          <img src="sub.png" alt="" height="260" class="image" />
+          <img src="sub.png" alt="" height="198" class="image" />
           <button type="button" class="btn btn-primary btn-xs">Add to Cart</button>
         </li>
       </ul>
