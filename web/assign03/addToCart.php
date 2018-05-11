272fc1eb0de55fa4916ccb837e05ff699f60e903
Klenton Stone <sto10026@byui.edu>
2018-05-11 12:31:42 -0600

    jquery working

--- web/assign03/addToCart.php
+++ web/assign03/addToCart.php
@@ -2,9 +2,9 @@
 
 	var_dump($_POST);
 
-	/*$idAssoc = [1 => "R/C Truck, $200.00", 2 => "White R/C Truck $200.00", 3 => "R/C Boat, $150.00", 4 => "R/C Plane, $400.00", 5 => "R/C Excavator, $150.00", 6 => "R/C Drone, $900.00", 7 => "R/C Tank, $160.00", 8 => "R/C Submarine, $400.00"];
+	$idAssoc = [1 => "R/C Truck, $200.00", 2 => "White R/C Truck $200.00", 3 => "R/C Boat, $150.00", 4 => "R/C Plane, $400.00", 5 => "R/C Excavator, $150.00", 6 => "R/C Drone, $900.00", 7 => "R/C Tank, $160.00", 8 => "R/C Submarine, $400.00"];
 
-	*/
+	
 
 	//$_SESSION['name'] = $_POST['name'];
 	//$_SESSION['price'] = $_POST['price'];
