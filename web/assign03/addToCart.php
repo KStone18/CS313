cf162ec92007f1d5247b1efcb622ed56e741b7b3
Klenton Stone <sto10026@byui.edu>
2018-05-11 11:14:59 -0600

    jquery working

--- web/assign03/addToCart.php
+++ web/assign03/addToCart.php
@@ -1,7 +1,8 @@
 <?php
 	var_dump($_POST);
 
-	$_SESSION['name'] = $_POST['name'];
-	$_SESSION['price'] = $_POST['price'];
-	echo "it worked i made it" . $_SESSION["name"];
+	$_SESSION['cart'] = array($_POST['name'], $_POST['price'])
+	
+
+	echo "it worked i made it" . $_SESSION['cart'][0];
 ?>
