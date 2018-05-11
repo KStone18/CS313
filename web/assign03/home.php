a87cbdac8ce1bf89e3492ee3d91625e3c095c0f4
Klenton Stone <sto10026@byui.edu>
2018-05-10 20:49:59 -0600

    adding image for cart

--- web/assign03/home.php
+++ web/assign03/home.php
@@ -10,52 +10,95 @@
         <li> 
           <div class="title">Item 1</div> 
           <img src="car1.png" alt=""  height="207" class="image" />
-          <button type="button" class="btn btn-primary btn-xs buttonCSS" >Add to Cart</button>
+          <form name="Item1" action="cart.php" method="POST" id="form1">
+            <input id="productQuantity" type="hidden" name="quantity" value="1"/>
+            <input id="productId" type="hidden" name="product_id" value="1"/>
+            <input id="productName" type="hidden" name="product_name" value="R/C Truck"/>
+            <input id="productPrice" type="hidden" name="product_price" value="200.00"/>
+            <button type="button" class="btn btn-primary btn-xs buttonCSS" >Add to Cart</button>
+          </form>
         </li>
         <li> 
           <div class="title">Item 2</div> 
           <img src="car2.png" alt="" height="207" class="image" /> 
-          <button type="button" class="btn btn-primary btn-xs buttonCSS" >Add to Cart</button>
+          <form name="Item1" action="cart.php" method="POST" id="form1">
+            <input id="productQuantity" type="hidden" name="quantity" value="1"/>
+            <input id="productId" type="hidden" name="product_id" value="1"/>
+            <input id="productName" type="hidden" name="product_name" value="R/C Truck"/>
+            <input id="productPrice" type="hidden" name="product_price" value="200.00"/>
+            <button type="button" class="btn btn-primary btn-xs buttonCSS" >Add to Cart</button>
+          </form>
         </li>
         <li> 
           <div class="title">Item 3</div> 
           <img src="Boat.png" alt="" height="207" class="image" />
-          <button type="button" class="btn btn-primary btn-xs buttonCSS">Add to Cart</button>
+          <form name="Item1" action="cart.php" method="POST" id="form1">
+            <input id="productQuantity" type="hidden" name="quantity" value="1"/>
+            <input id="productId" type="hidden" name="product_id" value="1"/>
+            <input id="productName" type="hidden" name="product_name" value="R/C Truck"/>
+            <input id="productPrice" type="hidden" name="product_price" value="200.00"/>
+            <button type="button" class="btn btn-primary btn-xs buttonCSS">Add to Cart</button>
         </li>
         <li> 
           <div class="title">Item 4</div> 
           <img src="Jet.png" alt="" height="207" class="image" />
-          <button type="button" class="btn btn-primary btn-xs buttonCSS">Add to Cart</button>
+          <form name="Item1" action="cart.php" method="POST" id="form1">
+            <input id="productQuantity" type="hidden" name="quantity" value="1"/>
+            <input id="productId" type="hidden" name="product_id" value="1"/>
+            <input id="productName" type="hidden" name="product_name" value="R/C Truck"/>
+            <input id="productPrice" type="hidden" name="product_price" value="200.00"/>
+            <button type="button" class="btn btn-primary btn-xs buttonCSS">Add to Cart</button>
+          </form>
         </li>
         <li> 
           <div class="title">Item 5</div> 
           <img src="excavator.png" alt="" height="207" class="image" />
-          <button type="button" class="btn btn-primary btn-xs buttonCSS">Add to Cart</button>
+          <form name="Item1" action="cart.php" method="POST" id="form1">
+            <input id="productQuantity" type="hidden" name="quantity" value="1"/>
+            <input id="productId" type="hidden" name="product_id" value="1"/>
+            <input id="productName" type="hidden" name="product_name" value="R/C Truck"/>
+            <input id="productPrice" type="hidden" name="product_price" value="200.00"/>
+            <button type="button" class="btn btn-primary btn-xs buttonCSS">Add to Cart</button>
+          </form>
         </li>
         <li> 
           <div class="title">Item 6</div> 
           <img src="Quadcopter.png" alt="" height="207" class="image" />
-          <button type="button" class="btn btn-primary btn-xs buttonCSS" >Add to Cart</button>
+          <form name="Item1" action="cart.php" method="POST" id="form1">
+            <input id="productQuantity" type="hidden" name="quantity" value="1"/>
+            <input id="productId" type="hidden" name="product_id" value="1"/>
+            <input id="productName" type="hidden" name="product_name" value="R/C Truck"/>
+            <input id="productPrice" type="hidden" name="product_price" value="200.00"/>
+            <button type="button" class="btn btn-primary btn-xs buttonCSS" >Add to Cart</button>
+          </form>
         </li>
         <li> 
           <div class="title">Item 7</div> 
           <img src="tank.png" alt="" height="207" class="image" />
-          <button type="button" class="btn btn-primary btn-xs buttonCSS" >Add to Cart</button>
+          <form name="Item1" action="cart.php" method="POST" id="form1">
+            <input id="productQuantity" type="hidden" name="quantity" value="1"/>
+            <input id="productId" type="hidden" name="product_id" value="1"/>
+            <input id="productName" type="hidden" name="product_name" value="R/C Truck"/>
+            <input id="productPrice" type="hidden" name="product_price" value="200.00"/>
+            <button type="button" class="btn btn-primary btn-xs buttonCSS" >Add to Cart</button>
+          </form>
         </li>
         <li> 
           <div class="title">Item 8</div> 
           <img src="sub.png" alt="" height="207" class="image" />
-          <button type="button" class="btn btn-primary btn-xs buttonCSS" >Add to Cart</button>
+          <form name="Item1" action="cart.php" method="POST" id="form1">
+            <input id="productQuantity" type="hidden" name="quantity" value="1"/>
+            <input id="productId" type="hidden" name="product_id" value="1"/>
+            <input id="productName" type="hidden" name="product_name" value="R/C Truck"/>
+            <input id="productPrice" type="hidden" name="product_price" value="200.00"/>
+            <button type="button" class="btn btn-primary btn-xs buttonCSS" >Add to Cart</button>
+          </form>
         </li>
       </ul>
     </div>
   </div>
 
- <div style="height: 300px">
-  <input id="productQuantity" type="hidden" name="quantity" value="1"/>
- <input id="productId" type="hidden" name="product_id" value="91801160"/>
- <input id="productName" type="hidden" name="product_name" value="Red Hat"/>
- </div>
+
 </body>
 </html>
 
