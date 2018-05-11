a76539b7fc29a6427601cc8c8a6133e1a9174db6
Klenton Stone <sto10026@byui.edu>
2018-05-10 19:23:44 -0600

    adding image for cart

--- web/assign03/header.php
+++ web/assign03/header.php
@@ -3,6 +3,7 @@
       <meta name="viewport" content="width=device-width" />
       <link rel="stylesheet" href="newMain.css" type="text/css" charset="utf-8">
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <script src="cart.js"></script>
    </head>
    <body>
       <?php include('navbar.php'); ?>
--- web/assign03/home.php
+++ web/assign03/home.php
@@ -10,42 +10,43 @@
         <li> 
           <div class="title">Item 1</div> 
           <img src="car1.png" alt=""  height="207" class="image" />
-          <button type="button" class="btn btn-primary btn-xs buttonCSS">Add to Cart</button>
+          <button type="button" class="btn btn-primary btn-xs buttonCSS" 
+                  onclick="addToCart()" id="add1">Add to Cart</button>
         </li>
         <li> 
           <div class="title">Item 2</div> 
           <img src="car2.png" alt="" height="207" class="image" /> 
-          <button type="button" class="btn btn-primary btn-xs buttonCSS">Add to Cart</button>
+          <button type="button" class="btn btn-primary btn-xs buttonCSS" onclick="addToCart()">Add to Cart</button>
         </li>
         <li> 
           <div class="title">Item 3</div> 
           <img src="Boat.png" alt="" height="207" class="image" />
-          <button type="button" class="btn btn-primary btn-xs buttonCSS">Add to Cart</button>
+          <button type="button" class="btn btn-primary btn-xs buttonCSS" onclick="addToCart()">Add to Cart</button>
         </li>
         <li> 
           <div class="title">Item 4</div> 
           <img src="Jet.png" alt="" height="207" class="image" />
-          <button type="button" class="btn btn-primary btn-xs buttonCSS">Add to Cart</button>
+          <button type="button" class="btn btn-primary btn-xs buttonCSS" onclick="addToCart()">Add to Cart</button>
         </li>
         <li> 
           <div class="title">Item 5</div> 
           <img src="excavator.png" alt="" height="207" class="image" />
-          <button type="button" class="btn btn-primary btn-xs buttonCSS">Add to Cart</button>
+          <button type="button" class="btn btn-primary btn-xs buttonCSS" onclick="addToCart()">Add to Cart</button>
         </li>
         <li> 
           <div class="title">Item 6</div> 
           <img src="Quadcopter.png" alt="" height="207" class="image" />
-          <button type="button" class="btn btn-primary btn-xs buttonCSS">Add to Cart</button>
+          <button type="button" class="btn btn-primary btn-xs buttonCSS" onclick="addToCart()">Add to Cart</button>
         </li>
         <li> 
           <div class="title">Item 7</div> 
           <img src="tank.png" alt="" height="207" class="image" />
-          <button type="button" class="btn btn-primary btn-xs buttonCSS">Add to Cart</button>
+          <button type="button" class="btn btn-primary btn-xs buttonCSS" onclick="addToCart()">Add to Cart</button>
         </li>
         <li> 
           <div class="title">Item 8</div> 
           <img src="sub.png" alt="" height="207" class="image" />
-          <button type="button" class="btn btn-primary btn-xs buttonCSS">Add to Cart</button>
+          <button type="button" class="btn btn-primary btn-xs buttonCSS" onclick="addToCart()">Add to Cart</button>
         </li>
       </ul>
     </div>
