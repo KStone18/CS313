40eb1fee5f8226009b6b426bbdba350fa54295b4
Klenton Stone <sto10026@byui.edu>
2018-05-10 20:06:51 -0600

    adding image for cart

--- web/assign03/cart.js
+++ web/assign03/cart.js
@@ -3,4 +3,29 @@ function addToCart() {
 	document.getElementById('add1').onclick = function() {
 		alert("button was clicked")
 	}
+
+	document.getElementById('add2').onclick = function() {
+		alert("button was clicked")
+	}
+
+	document.getElementById('add4').onclick = function() {
+		alert("button was clicked")
+	}
+
+	document.getElementById('add5').onclick = function() {
+		alert("button was clicked")
+	}
+
+	document.getElementById('add6').onclick = function() {
+		alert("button was clicked")
+	}
+
+	document.getElementById('add7').onclick = function() {
+		alert("button was clicked")
+	}
+
+	document.getElementById('add8').onclick = function() {
+		alert("button was clicked")
+	}
+
 }
--- web/assign03/home.php
+++ web/assign03/home.php
@@ -1,3 +1,7 @@
+<?php
+<!-- Start the session -->
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,47 +14,65 @@
         <li> 
           <div class="title">Item 1</div> 
           <img src="car1.png" alt=""  height="207" class="image" />
+
           <button type="button" class="btn btn-primary btn-xs buttonCSS" 
-                  onclick="addToCart()" id="add1">Add to Cart</button>
+                  >Add to Cart</button>
         </li>
         <li> 
           <div class="title">Item 2</div> 
           <img src="car2.png" alt="" height="207" class="image" /> 
-          <button type="button" class="btn btn-primary btn-xs buttonCSS" onclick="addToCart()">Add to Cart</button>
+          <button type="button" class="btn btn-primary btn-xs buttonCSS" 
+                  >Add to Cart</button>
         </li>
         <li> 
           <div class="title">Item 3</div> 
           <img src="Boat.png" alt="" height="207" class="image" />
-          <button type="button" class="btn btn-primary btn-xs buttonCSS" onclick="addToCart()">Add to Cart</button>
+          <button type="button" class="btn btn-primary btn-xs buttonCSS" 
+                  >Add to Cart</button>
         </li>
         <li> 
           <div class="title">Item 4</div> 
           <img src="Jet.png" alt="" height="207" class="image" />
-          <button type="button" class="btn btn-primary btn-xs buttonCSS" onclick="addToCart()">Add to Cart</button>
+          <button type="button" class="btn btn-primary btn-xs buttonCSS" 
+                  >Add to Cart</button>
         </li>
         <li> 
           <div class="title">Item 5</div> 
           <img src="excavator.png" alt="" height="207" class="image" />
-          <button type="button" class="btn btn-primary btn-xs buttonCSS" onclick="addToCart()">Add to Cart</button>
+          <button type="button" class="btn btn-primary btn-xs buttonCSS" 
+                  >Add to Cart</button>
         </li>
         <li> 
           <div class="title">Item 6</div> 
           <img src="Quadcopter.png" alt="" height="207" class="image" />
-          <button type="button" class="btn btn-primary btn-xs buttonCSS" onclick="addToCart()">Add to Cart</button>
+          <button type="button" class="btn btn-primary btn-xs buttonCSS" 
+                  >Add to Cart</button>
         </li>
         <li> 
           <div class="title">Item 7</div> 
           <img src="tank.png" alt="" height="207" class="image" />
-          <button type="button" class="btn btn-primary btn-xs buttonCSS" onclick="addToCart()">Add to Cart</button>
+          <button type="button" class="btn btn-primary btn-xs buttonCSS" 
+                  >Add to Cart</button>
         </li>
         <li> 
           <div class="title">Item 8</div> 
           <img src="sub.png" alt="" height="207" class="image" />
-          <button type="button" class="btn btn-primary btn-xs buttonCSS" onclick="addToCart()">Add to Cart</button>
+          <button type="button" class="btn btn-primary btn-xs buttonCSS" 
+                  >Add to Cart</button>
         </li>
       </ul>
     </div>
   </div>
+
+  <input id="productQuantity" type="hidden" name="quantity" value="1"/>
+ <input id="productId" type="hidden" name="product_id" value="91801160"/>
+ <input id="productName" type="hidden" name="product_name" value="Red Hat"/>
+
+<?php
+  session_unset();
+
+  session_destroy();
+?>
 
 </body>
 </html>
