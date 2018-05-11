c22ceee07f23025060251a81ba6cf8c1d50df693
Klenton Stone <sto10026@byui.edu>
2018-05-11 10:15:58 -0600

    adding image for cart

--- web/assign03/cart.js
+++ web/assign03/cart.js
@@ -1,135 +1,15 @@
 function addToCart() {
-	alert("in function");
-	//document.getElementById('btn1').onclick = function() {
-		alert("button 1 was clicked");
-		var request;
-		$("#form1").submit(function(event) {
-			event.preventDefault();
 
-			if (request) {
-				request.abort();
-			}
+	$(document).ready(function() {
+		$("button").click(function() {
+			$.post("addToCart.php", 
+			{
+				product: productName;
+				price: productPrice;
 
-			var $form = $(this);
-
-			var $inputs = $form.find("input, select, button, textarea");
-
-			var serializedData = $form.serialize();
-
-			$inputs.prop("disabled", true);
-
-			request = $.ajax({
-				url: "addToCart.php",
-				type: "post",
-				data: serializedData
-			});
-
-			request.done(function (response, textStatus, jqXHR) {
-				console.log(serializedData);
-			});
-
-			request.fail(function (jqXHR, textStatus, errorThrown) {
-				console.error(
-					"The following error occurred: " + textStatus, errorThrown
-					);
-			});
-
-			request.always(function() {
-				$inputs.prop("disabled", false);
-			});
-		});
-		
-	//};
-
-	/*document.getElementById('btn2').onclick = function() {
-		alert("button 2 was clicked");
-		var request;
-		$("#form2").submit(function(event) {
-			event.preventDefault();
-
-			if (request) {
-				request.abort();
-			}
-
-			var $form = $(this);
-
-			var $inputs = $form.find("input, select, button, textarea");
-
-			var serializedData = $form.serialize();
-
-			$inputs.prop("disabled", true);
-
-			request = $.ajax({
-				url: "addToCart.php",
-				type: "post",
-				data: serializedData
-			});
-
-			request.done(function (response, textStatus, jqXHR) {
-				console.log(serializedData);
-			});
-
-			request.fail(function (jqXHR, textStatus, errorThrown) {
-				console.error(
-					"The following error occurred: " + textStatus, errorThrown
-					);
-			});
-
-			request.always(function() {
-				$inputs.prop("disabled", false);
-			});
-		});
-	
-	};*/
-	
-
-	// var request;
-	// $(id).submit(function(event) {
-	// 	event.preventDefault();
-
-	// 	if (request) {
-	// 		request.abort();
-	// 	}
-
-	// 	var $form = $(this);
-
-	// 	var $inputs = $form.find("input, select, button, textarea");
-
-	// 	var serializedData = $form.serialize();
-
-	// 	$inputs.prop("disabled", true);
-
-	// 	request = $.ajax({
-	// 		url: "addToCart.php",
-	// 		type: "post",
-	// 		data: serializedData
-	// 	});
-
-	// 	request.done(function (response, textStatus, jqXHR) {
-	// 		console.log(serializedData);
-	// 	});
-
-	// 	request.fail(function (jqXHR, textStatus, errorThrown) {
-	// 		console.error(
-	// 			"The following error occurred: " + textStatus, errorThrown
-	// 			);
-	// 	});
-
-	// 	request.always(function() {
-	// 		$inputs.prop("disabled", false);
-	// 	});
-	// });
-
-	// $(document).ready(function() {
-	// 	$("button").click(function() {
-	// 		$.post("addToCart.php", 
-	// 		{
-	// 			product: productName;
-	// 			price: productPrice;
-
-	// 		})
-	// 	})
-	// })
+			})
+		})
+	})
 	
 }
 
--- web/assign03/home.php
+++ web/assign03/home.php
@@ -10,89 +10,56 @@
         <li> 
           <div class="title">Item 1</div> 
           <img src="car1.png" alt=""  height="207" class="image" />
-          <form name="Item1" method="POST" id="form1">
-            <input id="productQuantity" type="hidden" name="quantity" value="1"/>
-            <input id="productName" type="hidden" name="product_name" value="R/C Truck"/>
-            <input id="productPrice" type="hidden" name="product_price" value="200.00"/>
-            <input id="btn1" type="submit" class="btn btn-primary btn-xs buttonCSS" 
-                   name="button1" value="Add To Cart" onclick="addToCart()"></>
-          </form>
+          <button id="btn1" class="btn btn-primary btn-xs buttonCSS" 
+                  name="button1" onclick="addToCart(\"R/C Truck\", 200.00)">Add To Cart</button>
         </li>
         <li> 
           <div class="title">Item 2</div> 
           <img src="car2.png" alt="" height="207" class="image" /> 
-          <form name="Item1" method="POST" id="form2">
-            <input id="productQuantity" type="hidden" name="quantity" value="1"/>
-            <input id="productName" type="hidden" name="product_name" value="White R/C Truck"/>
-            <input id="productPrice" type="hidden" name="product_price" value="200.00"/>
-            <input id="btn2" type="submit" class="btn btn-primary btn-xs buttonCSS" 
-                   name="button2" value="Add To Cart" onclick="addToCart()"></>
-          </form>
+          <button id="btn2" class="btn btn-primary btn-xs buttonCSS" 
+                  name="button2" onclick="addToCart(\"White R/C Truck\", 200.00)">Add To Cart</button>
+         
         </li>
         <li> 
           <div class="title">Item 3</div> 
           <img src="Boat.png" alt="" height="207" class="image" />
-          <form name="Item1" method="POST" id="form3">
-            <input id="productQuantity" type="hidden" name="quantity" value="1"/>
-            <input id="productName" type="hidden" name="product_name" value="R/C Boat"/>
-            <input id="productPrice" type="hidden" name="product_price" value="120.00"/>
-            <input id="btn3" type="submit" class="btn btn-primary btn-xs buttonCSS" 
-                   name="button3" value="Add To Cart" onclick="addToCart()"></>
+          <button id="btn3" class="btn btn-primary btn-xs buttonCSS" 
+                  name="button3"  onclick="addToCart(\"R/C Boat", 150.00)">Add To Cart</button>
         </li>
         <li> 
           <div class="title">Item 4</div> 
           <img src="Jet.png" alt="" height="207" class="image" />
-          <form name="Item1" method="POST" id="form4">
-            <input id="productQuantity" type="hidden" name="quantity" value="1"/>
-            <input id="productName" type="hidden" name="product_name" value="R/C Plane"/>
-            <input id="productPrice" type="hidden" name="product_price" value="400.00"/>
-            <input id="btn4" type="submit" class="btn btn-primary btn-xs buttonCSS" 
-                   name="button4" value="Add To Cart" onclick="addToCart()"></>
-          </form>
+          <button id="btn4" class="btn btn-primary btn-xs buttonCSS" 
+                  name="button4" onclick="addToCart(\"R/C Plane\", 400.00)">Add To Cart</button>
+        
         </li>
         <li> 
           <div class="title">Item 5</div> 
           <img src="excavator.png" alt="" height="207" class="image" />
-          <form name="Item1" method="POST" id="form5">
-            <input id="productQuantity" type="hidden" name="quantity" value="1"/>
-            <input id="productName" type="hidden" name="product_name" value="R/C Excavator"/>
-            <input id="productPrice" type="hidden" name="product_price" value="150.00"/>
-            <input id="btn5" type="submit" class="btn btn-primary btn-xs buttonCSS" 
-                   name="button5" value="Add To Cart" onclick="addToCart()"></>
-          </form>
+          <button id="btn5" class="btn btn-primary btn-xs buttonCSS" 
+                  name="button5" onclick="addToCart(\"R/C Excavator\", 150.00)">Add To Cart</button>
+          
         </li>
         <li> 
           <div class="title">Item 6</div> 
           <img src="Quadcopter.png" alt="" height="207" class="image" />
-          <form name="Item1" method="POST" id="form6">
-            <input id="productQuantity" type="hidden" name="quantity" value="1"/>
-            <input id="productName" type="hidden" name="product_name" value="R/C Drone"/>
-            <input id="productPrice" type="hidden" name="product_price" value="900.00"/>
-            <input id="btn6" type="submit" class="btn btn-primary btn-xs buttonCSS" 
-                   name="button6" value="Add To Cart" onclick="addToCart()"></>
-          </form>
+          <button id="btn6" class="btn btn-primary btn-xs buttonCSS" 
+                  name="button6" onclick="addToCart(\"R/C Drone\", 900.00)">Add To Cart</button>
+          
         </li>
         <li> 
           <div class="title">Item 7</div> 
           <img src="tank.png" alt="" height="207" class="image" />
-          <form name="Item1" method="POST" id="form7">
-            <input id="productQuantity" type="hidden" name="quantity" value="1"/>
-            <input id="productName" type="hidden" name="product_name" value="R/C Tank"/>
-            <input id="productPrice" type="hidden" name="product_price" value="160.00"/>
-            <input id="btn7" type="submit" class="btn btn-primary btn-xs buttonCSS" 
-                   name="button7" value="Add To Cart" onclick="addToCart()"></>
-          </form>
+          <button id="btn7" class="btn btn-primary btn-xs buttonCSS" 
+                  name="button7" onclick="addToCart(\"R/C Tank\", 160.00)">Add To Cart</button>
+          
         </li>
         <li> 
           <div class="title">Item 8</div> 
           <img src="sub.png" alt="" height="207" class="image" />
-          <form name="Item1" method="POST" id="form8">
-            <input id="productQuantity" type="hidden" name="quantity" value="1"/>
-            <input id="productName" type="hidden" name="product_name" value="R/C Submarine"/>
-            <input id="productPrice" type="hidden" name="product_price" value="400.00"/>
-            <input id="btn8" type="submit" class="btn btn-primary btn-xs buttonCSS" 
-                   name="button8" value="Add To Cart" onclick="addToCart()"></>
-          </form>
+          <button id="btn8" class="btn btn-primary btn-xs buttonCSS" 
+                  name="button8" onclick="addToCart(\"R/C Submarine\", 400.00)">Add To Cart</>
+          
         </li>
       </ul>
     </div>
