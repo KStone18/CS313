73419d32be0620d817a50e4e5be25a9f1817496d
Klenton Stone <sto10026@byui.edu>
2018-05-10 19:26:04 -0600

    adding image for cart

--- web/assign03/cart.js
+++ web/assign03/cart.js
@@ -1,4 +1,6 @@
+function addToCart() {
 
-document.getElementById('add1').onclick = function() {
-	alert("button was clicked")
+	document.getElementById('add1').onclick = function() {
+		alert("button was clicked")
+	}
 }
