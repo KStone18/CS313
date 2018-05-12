c371b66d6871e44ded719df23f5fba89407beb9f
Klenton Stone <sto10026@byui.edu>
2018-05-12 14:46:07 -0600

    adding image for cart

--- web/assign03/cart.php
+++ web/assign03/cart.php
@@ -11,10 +11,11 @@
   	<div  class="shopCart">
       
 	       <?php
-
+         $totalPrice = 0;
          foreach ($_SESSION['cart'] as $id) {
           echo "<button class=\"btn btn-primary btn-sm buttonCSS\" onclick=\"removeFromCart('".$id."')\">Remove From Cart</button> ";
           echo "<span class=\"text\">{$idAssoc[$id]['name']}     \${$idAssoc[$id]['price']}.00</span><br>";
+          $totalPrice += $idAssoc[$id]["price"];
 
         }
         
@@ -22,6 +23,7 @@
 
         ?>
 	     <button id="myBtn" class="btn btn-primary btn-sm buttonCSS" onclick="redirect()">Checkout</button>
+       <?php echo $totalPrice . "00" ?>
    
 	 </div>
   </div>
