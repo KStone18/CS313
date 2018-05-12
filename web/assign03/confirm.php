d0d9e0e68d8a7dc053259a7eeefe94b06d07c28f
Klenton Stone <sto10026@byui.edu>
2018-05-12 17:04:39 -0600

    adding image for cart

---
+++ web/assign03/confirm.php
@@ -0,0 +1,27 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <title> Shopping Cart </title>
+  <?php
+  include('header.php');
+  include('assocArray.php');
+  ?>
+  <div class="screen">
+    <div  class="shopCart">
+      
+         <?php
+         $totalPrice = 0;
+         foreach ($_SESSION['cart'] as $id) {
+          echo "<button class=\"btn btn-primary btn-sm buttonCSS\" onclick=\"removeFromCart('".$id."')\">Remove From Cart</button> ";
+          echo "<span class=\"text\">{$idAssoc[$id]['name']}     <span class=\"textPrice\">\${$idAssoc[$id]['price']}.00</span></span><br>";
+          $totalPrice += $idAssoc[$id]["price"];
+
+        }
+        ?>
+
+        
+   </div>
+  </div>
+</body>
+</html>
