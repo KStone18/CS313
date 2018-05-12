460daaba1926606f552d1b9d61c857ed9e1c8452
Klenton Stone <sto10026@byui.edu>
2018-05-12 10:36:12 -0600

    begin printing of item to cart

--- web/assign03/cart.php
+++ web/assign03/cart.php
@@ -9,24 +9,26 @@
   ?>
   <div class="screen">
   	<div  class="shopCart">
+      <table>
+	       <?php
+  // 		//wrap in html tags
+  //   	foreach ($_SESSION['cart'] as $id) {
+	 // 		echo "Id=" . $id . ", Value=" . $idAssoc[$id]["name"] . $idAssoc[$id]["price"];
+	 // 		echo "<br>";
 
-	<?php
-  		//wrap in html tags
-    	foreach ($_SESSION['cart'] as $id) {
-	 		echo "Id=" . $id . ", Value=" . $idAssoc[$id]["name"] . $idAssoc[$id]["price"];
-	 		echo "<br>";
+		// }
 
-		}
-  foreach ($_SESSION['cart'] as $id) {
-    echo "<tr>";
-    echo "<td><button onclick=\"removeFromCart(".$id.")\">Delete</button></td>";
-    echo "<td class='descript'>".$services[$id]['name']."</td>";
-    echo "<td>\$".$services[$id]['price']."</td>";
-    echo "</tr>";
-    $totalPrice += $services[$id]["price"];
-	?>
-
-	</div>
+        foreach ($_SESSION['cart'] as $id) {
+         echo "<tr>";
+         echo "<td><button onclick=\"removeFromCart(".$id.")\">Delete</button></td>";
+         echo "<td class='descript'>".$services[$id]['name']."</td>";
+         echo "<td>\$".$services[$id]['price']."</td>";
+         echo "</tr>";
+        };
+    //$totalPrice += $services[$id]["price"];
+	   ?>
+    </table>
+	 </div>
   </div>
 </body>
 </html>
