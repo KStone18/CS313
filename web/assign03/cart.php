f07f575b6839fcff6ef7865aba11a695ea3b0131
Klenton Stone <sto10026@byui.edu>
2018-05-12 14:02:51 -0600

    adding image for cart

--- web/assign03/cart.php
+++ web/assign03/cart.php
@@ -14,7 +14,7 @@
 
          foreach ($_SESSION['cart'] as $id) {
           echo "<button class=\"btn btn-primary btn-sm buttonCSS\" onclick=\"removeFromCart('".$id."')\">Remove From Cart</button> ";
-          echo "<p style=\"color: white;\">"$idAssoc[$id]['name'] . " " . $idAssoc[$id]['price']  . "</p><br>";
+          echo "<p>{$idAssoc[$id]['name']} : {$idAssoc[$id]['price']}</p><br>";
 
         }
         
