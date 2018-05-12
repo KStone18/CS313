c7ae1eb271af95e12782786aed909eb0697d3eaa
Klenton Stone <sto10026@byui.edu>
2018-05-12 11:25:39 -0600

    adding image for cart

--- web/assign03/removeFrom.php
+++ web/assign03/removeFrom.php
@@ -1,15 +1,13 @@
 <?php
 	session_start();
-	echo "here";
-
+	$id = $_POST['id']
 	if(isset($_SESSION['cart'])) {
-		$key = array_search($_POST['id'], $_SESSION['id']);
+		$key = array_search($id, $_SESSION['id']);
 		if ($key !== false)
 		{
 			unset($_SESSION['cart'][$key]);
+			$_SESSION['cart'] = array_values($_SESSION['cart']);
 		}
-
-		$_SESSION['cart'] = array_values($_SESSION['cart']);
 	}
 		
 	$numItems = sizeof($_SESSION['cart']);
