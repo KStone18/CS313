13e5a06454ee5a2434d493767008e188d85ed41b
Klenton Stone <sto10026@byui.edu>
2018-05-12 10:47:16 -0600

    begin printing of item to cart

--- web/assign03/cart.php
+++ web/assign03/cart.php
@@ -19,13 +19,13 @@
 		// }
          echo "does this work?"
 
-        // foreach ($_SESSION['cart'] as $id) {
-        //  echo "<tr>";
-        //  echo "<td><button onclick=\"removeFromCart(".$id.")\">Delete</button></td>";
+         foreach ($_SESSION['cart'] as $id) {
+          echo "<tr>";
+          echo "<td><button onclick=\"removeFromCart(".$id.")\">Delete</button></td>";
         //  echo "<td class="">".$idAssoc[$id]['name']."</td>";
         //  echo "<td>\$".$idAssoc[$id]['price']."</td>";
-        //  echo "</tr>";
-        // };
+          echo "</tr>";
+        }
     //$totalPrice += $services[$id]["price"];
 	   ?>
     </table>
