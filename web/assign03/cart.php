d84de1cd5b9c079fbc5d95ade50c2db73fefdf29
Klenton Stone <sto10026@byui.edu>
2018-05-11 23:58:18 -0600

    add session start to addtocart

--- web/assign03/cart.php
+++ web/assign03/cart.php
@@ -5,7 +5,21 @@
 <html lang="en">
 <head>
  <title> Shopping Cart </title>
-  <?php include('header.php'); ?>
-  
+  <?php 
+  include('header.php'); 
+  include('assocArray.php');
+  ?>
+
+
+
+  	<!--<?php
+  	//wrap in html tags 
+  	// foreach ($_SESSION['cart'] as $itemNumber => $value) {
+	 	// echo "Id=" . $itemNumber . ", Value=" . $value;
+	 	// echo "<br>";
+	 	
+	}
+	?>-->
+
 </body>
 </html>
