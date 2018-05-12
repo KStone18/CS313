322ac0865c535205b715417b17ab6d3f931245c6
Klenton Stone <sto10026@byui.edu>
2018-05-11 23:45:24 -0600

    add session start to addtocart

--- web/assign03/addToCart.php
+++ web/assign03/addToCart.php
@@ -6,32 +6,7 @@
 	}
 	 
 	array_push($_SESSION['cart'], $_POST['id']);
-
-	 
-	$idAssoc = ["1" => "R/C Truck, $200.00", "2" => "White R/C Truck $200.00", "3" => "R/C Boat, $150.00", "4" => "R/C Plane, $400.00", "5" => "R/C Excavator, $150.00", "6" => "R/C Drone, $900.00", "7" => "R/C Tank, $160.00", "8" => "R/C Submarine, $400.00"];
-
-	$count=0;
-
-	// foreach ($_SESSION['cart']['id'] as $itemNumber => $value) {
-	//  	echo "Id=" . $itemNumber . ", Value=" . $value;
-	//  	echo "<br>";
-	//  	$count++;
-	//  }
-
-
-
-
-
-	//var_dump($_SESSION['cart']);
-
-
-
-	// $idAssoc = [1 => "R/C Truck, $200.00", 2 => "White R/C Truck $200.00", 3 => "R/C Boat, $150.00", 4 => "R/C Plane, $400.00", 5 => "R/C Excavator, $150.00", 6 => "R/C Drone, $900.00", 7 => "R/C Tank, $160.00", 8 => "R/C Submarine, $400.00"];
-
+	$count++;
 	
-
-	//$_SESSION['name'] = $_POST['name'];
-	//$_SESSION['price'] = $_POST['price'];
-
-	echo $count; //. $_SESSION["name"];
+	echo $count; 
 ?>
--- web/assign03/cart.php
+++ web/assign03/cart.php
@@ -1,8 +1,20 @@
+<?php
+	session_start();
+?>	
 <!DOCTYPE html>
 <html lang="en">
 <head>
  <title> Shopping Cart </title>
   <?php include('header.php'); ?>
+  <?php include('assocArray.php'); ?>
 
+  	<?php
+  	//wrap in html tags 
+  	// foreach ($_SESSION['cart'] as $itemNumber => $value) {
+	 	// echo "Id=" . $itemNumber . ", Value=" . $value;
+	 	// echo "<br>";
+	 	
+	}
+	?>
 </body>
 </html>
