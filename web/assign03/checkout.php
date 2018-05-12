a62d3c28d9b1b8e51f40288fa6a4e2331c107d6a
Klenton Stone <sto10026@byui.edu>
2018-05-12 16:20:28 -0600

    adding image for cart

--- web/assign03/checkout.php
+++ web/assign03/checkout.php
@@ -23,29 +23,29 @@
        <?php echo "<span class=\"textTotal\"> Total: \$$totalPrice.00</span><br>"; ?>
      </div> 
 
-     <form onsubmit="return validateMyForm()" action="confirm.php" method="post">
+     <!-- <form onsubmit="return validateMyForm()" action="confirm.php" method="post">
 
-      <div class="form-group"> <!-- Full Name -->
+      <div class="form-group"> 
         <label for="full_name_id" class="control-label">Full Name</label>
         <input type="text" class="form-control" id="full_name_id" name="full_name" placeholder="John Deer">
       </div>  
 
-      <div class="form-group"> <!-- Street 1 -->
+      <div class="form-group"> 
         <label for="street1_id" class="control-label">Street Address 1</label>
         <input type="text" class="form-control" id="street1_id" name="street1" placeholder="Street address, P.O. box, company name, c/o">
       </div>          
               
-      <div class="form-group"> <!-- Street 2 -->
+      <div class="form-group"> 
         <label for="street2_id" class="control-label">Street Address 2</label>
         <input type="text" class="form-control" id="street2_id" name="street2" placeholder="Apartment, suite, unit, building, floor, etc.">
       </div>  
 
-      <div class="form-group"> <!-- City-->
+      <div class="form-group"> 
         <label for="city_id" class="control-label">City</label>
         <input type="text" class="form-control" id="city_id" name="city" placeholder="Smallville">
       </div>                  
               
-      <div class="form-group"> <!-- State Button -->
+      <div class="form-group"> 
         <label for="state_id" class="control-label">State</label>
           <select class="form-control" id="state_id">
             <option value="AL">Alabama</option>
@@ -102,16 +102,16 @@
           </select>         
         </div>
   
-        <div class="form-group"> <!-- Zip Code-->
+        <div class="form-group"> 
           <label for="zip_id" class="control-label">Zip Code</label>
           <input type="text" class="form-control" id="zip_id" name="zip" placeholder="#####">
         </div>    
   
-        <div class="form-group"> <!-- Submit Button -->
+        <div class="form-group"> 
           <button type="submit" class="btn btn-primary">Buy!</button>
         </div>     
   
-      </form> 
+      </form>  -->
   
 	 </div>
   </div>
