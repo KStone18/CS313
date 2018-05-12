a13624667cfe13ec785b443b0ec21e10e63fb902
Klenton Stone <sto10026@byui.edu>
2018-05-12 12:42:24 -0600

    adding image for cart

--- web/assign03/cart.php
+++ web/assign03/cart.php
@@ -9,29 +9,20 @@
   ?>
   <div class="screen">
   	<div  class="shopCart">
+      <table>
+	       <?php
 
-      foreach ($_SESSION['cart'] as $id) {
-         
-         echo "<span><button onclick=\"removeFromCart('".$id."')\">Delete</button><";
-         echo "\t $idAssoc[$id]['name']";
-         echo "\t $idAssoc[$id]['price']";
-         echo "</span>";
+        foreach ($_SESSION['cart'] as $id) {
+         echo "<tr>";
+         echo "<td><button onclick=\"removeFromCart('".$id."')\">Delete</button></td>";
+         echo "<td align='left'>".$idAssoc[$id]['name']."</td>";
+         echo "<td align='right'> \$".$idAssoc[$id]['price']."</td>";
+         echo "</tr>";
         }
-
-
-      <!-- <table> -->
-	       <!-- <?php
-
-        // foreach ($_SESSION['cart'] as $id) {
-        //  echo "<tr>";
-        //  echo "<td><button onclick=\"removeFromCart('".$id."')\">Delete</button></td>";
-        //  echo "<td class=".">".$idAssoc[$id]['name']."</td>";
-        //  echo "<td align=\"right\"> \$".$idAssoc[$id]['price']."</td>";
-        //  echo "</tr>";
-        // }
-    //$totalPrice += $services[$id]["price"];
-	   ?> -->
-    <!-- </table> -->
+        //$totalPrice += $services[$id]["price"];
+        ?>
+	  
+    </table> 
 	 </div>
   </div>
 </body>
