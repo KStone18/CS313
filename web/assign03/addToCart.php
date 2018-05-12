baa787ec6474fa0f78f5f10d9479321ff160f6e3
Klenton Stone <sto10026@byui.edu>
2018-05-11 22:53:38 -0600

    add session start to addtocart

--- web/assign03/addToCart.php
+++ web/assign03/addToCart.php
@@ -1,7 +1,15 @@
 <?php
 	session_start();
-	
-	var_dump($_POST);
+
+	if(empty($_SESSION['cart'])) {
+		$_SESSION['cart'] = array();
+	}
+
+	array_push($_SESSION['cart'], $_POST['id'])
+
+	var_dump($_SESSION['cart']);
+
+
 
 	$idAssoc = [1 => "R/C Truck, $200.00", 2 => "White R/C Truck $200.00", 3 => "R/C Boat, $150.00", 4 => "R/C Plane, $400.00", 5 => "R/C Excavator, $150.00", 6 => "R/C Drone, $900.00", 7 => "R/C Tank, $160.00", 8 => "R/C Submarine, $400.00"];
 
