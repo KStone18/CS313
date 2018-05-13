c3cbdffd23280fd35c2532278be45cb1161f1ed6
Klenton Stone <sto10026@byui.edu>
2018-05-12 18:50:16 -0600

    adding image for cart

--- web/assign03/assocArray.php
+++ web/assign03/assocArray.php
@@ -4,7 +4,7 @@ $idAssoc = array("1" => array("name" => "Black R/C Truck", "price" => 200.00),
 				 "3" => array("name" => "R/C Boat", "price" => 150.00),
 				 "4" => array("name" => "R/C Plane", "price" => 400.00),
 				 "5" => array("name" => "R/C Excavator", "price" => 150.00),
-				 "6" => array("name" => "R/C Drone", "price" => 900.00)
+				 "6" => array("name" => "R/C Drone", "price" => 900.00),
 				 "7" => array("name" => "R/C Tank", "price" => 160.00),
 				 "8" => array("name" => "R/C Submarine", "price" => 400.00));
 
