4e932ffdf97e4a72c00d4aa2c78c450aafc77bdc
Klenton Stone <sto10026@byui.edu>
2018-05-24 11:43:26 -0600

    adding main pages

--- web/assign04/home.php
+++ web/assign04/home.php
@@ -29,8 +29,8 @@
       <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
 
       <div class="clearfix">
-        <button type="button" class="btn btn-primary btn-md">Cancel</button>
-        <button type="submit" class="btn btn-primary btn-md">Sign Up</button>  
+        <button type="button" class="btn btn-primary btn-md cancelbtn">Cancel</button>
+        <button type="submit" class="btn btn-primary btn-md signupbtn">Sign Up</button>  
        
       </div>
     </div>
