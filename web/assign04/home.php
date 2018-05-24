393d368f750fc3d12d99da7197fb99c124876e72
Klenton Stone <sto10026@byui.edu>
2018-05-24 09:26:00 -0600

    adding main pages

---
+++ web/assign04/home.php
@@ -0,0 +1,19 @@
+<?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <title> abcd </title>
+  <?php include('header.php'); ?>
+  <div class="pTitle">
+  <div class="screen">
+    <div class="container">
+     
+    </div>
+  </div>
+
+
+</body>
+</html>
+
---
+++ web/assign04/navbar.php
@@ -0,0 +1,28 @@
+
+<nav class="navbar navbar-inverse navbar-fixed-top strip">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#"></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="home.php">Home</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Contact Us</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="cart.php">
+          <span class="glyphicon glyphicon-shopping-cart" id=""></span>
+
+        </a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
