4ba8aead083e4f3d304c9d7cb37cdb1660826aa8
Klenton Stone <sto10026@byui.edu>
2018-05-24 10:23:40 -0600

    adding main pages

---
+++ web/assign04/navbarA.php
@@ -0,0 +1,26 @@
+
+<nav class="navbar navbar-inverse navbar-fixed-top strip">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#"></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="home.php">Home</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Contact Us</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">
+        </a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span></a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Name of User</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
