edaa7fca02281a89c9277865e42ebd0deb418f03
Klenton Stone <sto10026@byui.edu>
2018-05-24 10:32:42 -0600

    adding main pages

--- web/assign04/navbarA.php
+++ web/assign04/navbarA.php
@@ -16,10 +16,25 @@
         <li><a href="#">Contact Us</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">
-        </a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span></a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Name of User</a></li>
+
+        <!-- <li>
+          <a href="#">
+            <span class="glyphicon glyphicon-user"></span> Name of User
+          </a>
+        </li> -->
+
+        <li class="nav-item dropdown">
+          <a href="#" class="nav-link dropdown-toggle" id="navDropDownLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <span class="glyphicon glyphicon-user"></span> Name Profile
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navDropDownLink">
+            <a class="dropdown-item" href="#">Preferences</a>
+          <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Logout</a>
+          </div>
+        </li>
+          
+
       </ul>
     </div>
   </div>
