427d968efc2e19dc2bc658894a012f6bf397dcf7
Klenton Stone <sto10026@byui.edu>
2018-05-24 09:37:53 -0600

    adding main pages

--- web/assign04/home.php
+++ web/assign04/home.php
@@ -6,14 +6,7 @@
 <head>
  <title> abcd </title>
   <?php include('header.php'); ?>
-  <div class="pTitle">
-    <div class="screen">
-      <div class="container">
-     
-      </div>
-    </div>
-  </div>
-
+ 
 
 </body>
 </html>
