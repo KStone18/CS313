b9900d2d0841fdcb762dd8d8112912a8b58840fa
Klenton Stone <sto10026@byui.edu>
2018-05-25 11:52:37 -0600

    adding more pages

--- web/assign04/profile.php
+++ web/assign04/profile.php
@@ -5,16 +5,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
- <title> </title>
+ <title> <?php ($db->query('SELECT username FROM app_user') as $name); echo "$name \'s Profile"; </title>
   <?php include('headerA.php'); ?>
 
   <div class="test"> 
-	
+	<?php 
+	$db->query('SELECT username FROM app_user') as $name; 
+	echo "$name" . "\'s Profile";
+	?>
   </div>
 
 
   <form action="" class="pForm">
- 	<input type="text" value="john"/>
  	<input type="text" value="john"/>
  	<input type="text" value="john"/>
  	<input type="text" value="john"/>
