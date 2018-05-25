55397fafa97126a1184ba1038293ae1bfa82466d
Klenton Stone <sto10026@byui.edu>
2018-05-25 13:14:41 -0600

    adding more pages

--- web/assign04/profile.php
+++ web/assign04/profile.php
@@ -10,20 +10,34 @@
  <title>  </title>
   <?php include('headerA.php'); ?>
 
-  <div class="test"> 
-	<h3>Hello </h3>
-  </div>
+  <form action="profile.php" method="POST" style="border:1px solid #ccc">
+    <div class="containerSign">
+      <h1>Sign Up</h1>
+      <p>Please fill in this form to create an account.</p>
+      <hr>
 
+      <label for="Username"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uName" required>
 
-  <form action="" class="pForm">
- 	<input type="text" value="john"/>
- 	<input type="text" value="john"/>
- 	<input type="text" value="john"/>
- 	<input type="text" value="john"/>
- 	<input type="text" value="john"/>
- 	<input type="text" value="john"/>
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required>
+
+      <label for="psw-repeat"><b>Repeat Password</b></label>
+      <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+      
+      <label>
+        <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
+      </label>
+      
+      <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+
+      <div class="clearfix">
+        <!-- <button type="button" class="btn btn-primary btn-md cancelbtn">Cancel</button> -->
+        <button type="submit" class="btn btn-primary btn-md signupbtn">Sign Up</button>  
+       
+      </div>
+    </div>
   </form>
-
   
 
 
