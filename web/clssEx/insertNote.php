ce0ea550b48f876c44b3feda731c8d069d21b7d9
Klenton Stone <sto10026@byui.edu>
2018-05-31 16:52:42 -0600

    adding code for class example

--- web/clssEx/insertNote.php
+++ web/clssEx/insertNote.php
@@ -1,26 +1,18 @@
 <?php
-	$courseId = htmlspecialchars($_POST["couse_id"]);
-	$date = htmlspecialchars($_POST["date"]);
-	$content = htmlspecialchars($_POST["content"]);
-
-	// echo "Course: $courseId\n";
-	// echo "Date: $date\n";
-	// echo "Content: $content\n";
-
-	require("data.php");
-
-	$db = get_db();
-
-	$query = 'INSERT INTO note (course_id, content, "date") VALUE (:courseID, :content, :"date")';
-	$statement = $db->prepare($query);
-	$statement.bindValue(":courseID", $courseID, PDO::PARAM_INT);
-	$statement.bindValue(":content", $content, PDO::PARAM_STR);
-	$statement.bindValue(":date", $date, PDO::PARAM_STR);
-
-	$statement->execute();
-
-	header("Location: courseDetails.php?course_id=$courseId");
-	die();
-	echo "It has worked";
-
+$courseId = htmlspecialchars($_POST["course_id"]);
+$date = htmlspecialchars($_POST["date"]);
+$content = htmlspecialchars($_POST["content"]);
+// echo "Course: $courseId\n";
+// echo "date: $date\n";
+// echo "content: $content\n";
+require("dbConnect.php");
+$db = get_db();
+$query = "INSERT INTO note (course_id, content, date) VALUES (:courseId, :content, :date)";
+$statement = $db->prepare($query);
+$statement->bindValue(":courseId", $courseId, PDO::PARAM_INT);
+$statement->bindValue(":content", $content, PDO::PARAM_STR);
+$statement->bindValue(":date", $date, PDO::PARAM_STR);
+$statement->execute();
+header("Location: courseDetails.php?course_id=$courseId");
+die();
 ?>
