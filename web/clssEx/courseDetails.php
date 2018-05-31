a133684e1c839fb73dc187edcef3d8f65dac931c
Klenton Stone <sto10026@byui.edu>
2018-05-31 17:00:33 -0600

    adding code for class example

--- web/clssEx/courseDetails.php
+++ web/clssEx/courseDetails.php
@@ -1,23 +1,15 @@
 <?php
 require("data.php");
-
 $courseId = htmlspecialchars($_GET["course_id"]);
-
 $db = get_db();
-
-
-$query = "SELECT name, number FROM course WHERE id = :id";
-
+$query = "SELECT name, number FROM course WHERE id=:id";
 $statement = $db->prepare($query);
 $statement->bindValue(":id", $courseId, PDO::PARAM_INT);
 $statement->execute();
 $row = $statement->fetch();
-
 $number = $row["number"];
 $name = $row["name"];
-
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -25,16 +17,17 @@ $name = $row["name"];
 </head>
 <body>
 
-	<?php 
-	echo "<h1>Showing Notes for: $number - $name</h1>";
-	?>
+<?php
+	echo "<h1>Showing notes for: $number - $name</h1>";
+?>
 
 <form action="insertNote.php" method="POST">
-	<input type="hidden" name="course_id" value="<?php echo $courseId;?>">
-	<input type="date" name="date"><br>
-	<textarea name="content" placeholder="Content"></textarea>
-	<br><br>
-	<input type="submit" value="Add Note"> 
+<input type="hidden" name="course_id" value="<?php echo $courseId; ?>">
+<input type="date" name="date"><br>
+<textarea name="content" placeholder="Content"></textarea>
+
+<br><br>
+<input type="submit" value="Add Note">
 </form>
 
 </body>
