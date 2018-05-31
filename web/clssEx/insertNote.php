9780a942351a80c472263513ab8a5591e4573364
Klenton Stone <sto10026@byui.edu>
2018-05-31 16:51:00 -0600

    adding code for class example

--- web/clssEx/insertNote.php
+++ web/clssEx/insertNote.php
@@ -3,15 +3,15 @@
 	$date = htmlspecialchars($_POST["date"]);
 	$content = htmlspecialchars($_POST["content"]);
 
-	echo "Course: $courseId\n";
-	echo "Date: $date\n";
-	echo "Content: $content\n";
+	// echo "Course: $courseId\n";
+	// echo "Date: $date\n";
+	// echo "Content: $content\n";
 
 	require("data.php");
 
 	$db = get_db();
 
-	$query = "INSERT INTO note (course_id, content, date) VALUE (:courseID, :content, :date)";
+	$query = 'INSERT INTO note (course_id, content, "date") VALUE (:courseID, :content, :"date")';
 	$statement = $db->prepare($query);
 	$statement.bindValue(":courseID", $courseID, PDO::PARAM_INT);
 	$statement.bindValue(":content", $content, PDO::PARAM_STR);
