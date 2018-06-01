defe0104a5e641184da9ba29a76ab9c30bde0ca0
Klenton Stone <sto10026@byui.edu>
2018-06-01 09:36:11 -0600

    working on addign name to profile

--- web/assign06/addInfo.php
+++ web/assign06/addInfo.php
@@ -13,24 +13,34 @@
       <p>Please fill in this form to create an account.</p>
       <hr>
 
-      <label for="Username"><b>Username</b></label>
-      <input type="text" placeholder="Enter Username" name="uName" required>
+      <label for="StreamName"><b>Stream Name</b></label>
+      <input type="text" placeholder="Enter Stream Name" name="streamName" required>
 
-      <label for="psw"><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="psw" required>
+      <label for="SiteName"><b>Site Name</b></label>
+      <input type="text" placeholder="Enter Site name" name="siteName" required>
 
-      <label for="psw-repeat"><b>Repeat Password</b></label>
+      <label for="Latitude"><b>Latitude</b></label>
+      <input type="text" placeholder="Enter Latitude" name="Latitude" required>
+
+      <label for="Longitude"><b>Longitude</b></label>
+      <input type="text" placeholder="Enter Longitude" name="Longitude" required>
+
+      <label for="descript"><b>Description of site</b></label>
+      <textarea name="descriptOfSite" placeholder="Add Description"></textarea>
+
+
+     <!--  <label for="psw-repeat"><b>Repeat Password</b></label>
       <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
-      
-      <label>
+       -->
+    <!--   <label>
         <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
       </label>
       
-      <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+      <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p> -->
 
       <div class="clearfix">
         <!-- <button type="button" class="btn btn-primary btn-md cancelbtn">Cancel</button> -->
-        <button type="submit" class="btn btn-primary btn-md signupbtn">Sign Up</button>  
+        <button type="submit" class="btn btn-primary btn-md signupbtn">Add Information</button>  
        
       </div>
     </div>
