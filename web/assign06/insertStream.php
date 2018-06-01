fefbc020ab17a2980bfd4c5a2ab1ae6cb2bb751f
Klenton Stone <sto10026@byui.edu>
2018-06-01 11:46:46 -0600

    working on addign name to profile

--- web/assign06/insertStream.php
+++ web/assign06/insertStream.php
@@ -1,5 +1,13 @@
 <?php
 	include('database.php');
 	$streamName = htmlspecialchars($_POST["streamName"]);
-	echo $streamName;
+	
+	$query = "INSERT INTO stream (name) VALUES (:name)";
+
+	$statement = $db->prepare($query);
+	$statement->bindValue(":name", $streamName, PDO::PARAM_STR);
+
+	$statement->execute();
+
+	echo "inserted into database"; 
 ?>
