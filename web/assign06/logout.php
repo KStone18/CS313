7b62ea06c6a0fc349f372728e4c397aa86c992ee
Klenton Stone <sto10026@byui.edu>
2018-06-01 20:39:42 -0600

    working on addign name to profile

--- web/assign06/logout.php
+++ web/assign06/logout.php
@@ -1,13 +1,13 @@
 <?php
 echo "here". $_SESSION["user_Name"] . " "; 
 
-if(isset($_SESSION["user_Name"])) {
+// if(isset($_SESSION["user_Name"])) {
 
-	echo "inside"
-	unset($_SESSION["user_Name"]);
+// 	echo "inside"
+// 	unset($_SESSION["user_Name"]);
 
-	session_destroy();
+// 	session_destroy();
 
-	header("Location: home.php");
-}
+// 	header("Location: home.php");
+// }
 ?>
