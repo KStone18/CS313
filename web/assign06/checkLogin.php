e82186e4762d856d8bba3e4b5ac719c27f244808
Klenton Stone <sto10026@byui.edu>
2018-06-01 18:43:28 -0600

    working on addign name to profile

--- web/assign06/checkLogin.php
+++ web/assign06/checkLogin.php
@@ -5,20 +5,20 @@ include('database.php');
 $userName = htmlspecialchars($_POST["uname"]);
 $password = htmlspecialchars($_POST["psw"]);
 
+echo $userName . " " . $password;
+ // foreach ($db->query('SELECT id, username, password FROM app_user') as $row) {
+ //    if ($userName = $row["username"] && $password = $row["password"]);
+ //       {
+ //         $SESSION["user_Name"] = $userName;
+ //         header("Location: profile.php");
 
- foreach ($db->query('SELECT id, username, password FROM app_user') as $row) {
-    if ($userName = $row["username"] && $password = $row["password"]);
-       {
-         $SESSION["user_Name"] = $userName;
-         header("Location: profile.php");
-
-       }
-       else 
-       {
-       	echo "You have not signed up. Please signup before continuing!";
-       	header("Location: home.php");
-       }
-  } 
+ //       }
+ //       else 
+ //       {
+ //       	echo "You have not signed up. Please signup before continuing!";
+ //       	header("Location: home.php");
+ //       }
+ //  } 
 
 
 ?>
